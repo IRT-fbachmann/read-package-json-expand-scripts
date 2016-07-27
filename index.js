1a1dcd383948b80037bd170e70eb272513c0eaee
Florian Bachmann <florian.bachmann@irt.de>
2016-07-27 17:09:15 +0200

    Release candidate

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 var scripts;
 var expscripts = {};
 
-module.exports = function (s) {
-	var scriptNames = Object.keys(s);
-	scripts = s;
+module.exports = function (data) {
+	var scriptNames = Object.keys(data.scripts);
+	scripts = data.scripts;
 	for(var i=0; i<scriptNames.length; i++)
 		expscripts[scriptNames[i]] = expandCommand(scripts[scriptNames[i]]);
-	return expscripts;
+	data.scripts = expscripts;
+	return data;
 }
 
 function expandCommand(cmd) {
   var scmd;
-  while( (scmd = /~([\w:-_#@]+)/g.exec(cmd)) !== null) {
+	while( (scmd = /~([\w:-_#@]+)/g.exec(cmd)) !== null) {
     cmd = cmd.replace(scmd[0], expandCommand(scripts[scmd[1]]));
   }
   return cmd;
